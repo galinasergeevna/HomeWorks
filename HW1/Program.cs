9d55bdda06da679616569bd45c9320df280f1df6
galina <galinasergeevnak@mail.ru>
2022-08-22 13:30:23 +0300

    completed the second task

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 Console.WriteLine("Input first number: ");
@@ -18,3 +18,33 @@ else
 {
     Console.WriteLine($"{num1} and {num2} are equal");
 }
+*/
+/*
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int maxnum = num1;
+
+if(num2 > maxnum)
+{ 
+    if(num2 > num3)
+    Console.WriteLine($"The maximum number = {num2} ");
+    else if(num3 > maxnum)
+    Console.WriteLine($"The maximum number = {num3} ");
+}
+else if(maxnum > num3)
+{  
+    Console.WriteLine($"The maximum number = {num1} ");
+}
+else
+{
+    Console.WriteLine($"The maximum number = {num3} ");
+}
+/*
+
+//
