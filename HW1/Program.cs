2d49331cfce8da2a527009a814b62f697d65c73c
galina <galinasergeevnak@mail.ru>
2022-08-22 14:58:49 +0300

    completed thrid task

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -45,6 +45,21 @@ else
 {
     Console.WriteLine($"The maximum number = {num3} ");
 }
+*/
 /*
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num%2 == 0)
+{
+   Console.WriteLine($"{num} is the even number"); 
+}
+else
+{
+    Console.WriteLine($"{num} is the odd number");
+}
+*/
 
 //
