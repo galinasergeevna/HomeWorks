ad64133238a2229203f8a7cec4bd4fe3b98339a8
galina <galinasergeevnak@mail.ru>
2022-08-22 16:17:34 +0300

    completed fourth task

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -62,4 +62,20 @@ else
 }
 */
 
-//
+/*
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Input integer positive number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+while(current <= number)
+{
+    if(current%2 == 0)
+    {
+        Console.Write(current+ " ");
+    }
+    current++;
+}
+*/
