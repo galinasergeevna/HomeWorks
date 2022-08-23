6b4c9936f570d59b095b38702cc5cb32ed9becae
galina <galinasergeevnak@mail.ru>
2022-08-23 21:02:12 +0300

    Completed first task

---
+++ HW2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Input three-digital number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int CutNumber(int number)
+{
+    int dec = number/10;
+
+    return dec%10;
+}
+int newNumber = CutNumber(number);
+Console.WriteLine($"Two gigital of {number} is {newNumber}");
+*/
+
+//
