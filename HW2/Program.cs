01a95ba19329fadf3759b9f300297fe7fa992722
galina <galinasergeevnak@mail.ru>
2022-08-26 14:53:54 +0300

    Complete second task

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -14,4 +14,20 @@ int newNumber = CutNumber(number);
 Console.WriteLine($"Two gigital of {number} is {newNumber}");
 */
 
-//
+/*
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void ThirdDigitNumber(int number)
+{ 
+  int ed = number % 10;
+  if (number > 99)
+    Console.WriteLine($"Third digit is: {ed}");
+  else
+    Console.WriteLine("Third digit missing");
+}
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ThirdDigitNumber(number);
+*/
+
+
